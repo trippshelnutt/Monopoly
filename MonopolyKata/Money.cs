a792c7f1925172b18463de99b3441220a2bf34db
Tripp Shelnutt <shelnutt77@gmail.com>
2020-02-28 23:58:51 -0500

    added money

---
+++ MonopolyKata/Money.cs
@@ -0,0 +1,28 @@
+﻿namespace MonopolyKata
+{
+
+    public readonly struct Money
+    {
+        public Money(int amount)
+        {
+            Amount = amount;
+        }
+
+        public int Amount { get; }
+
+        public override string ToString()
+        {
+            return $"Money: {Amount}";
+        }
+
+        public Money Add(Money money)
+        {
+            return new Money(Amount + money.Amount);
+        }
+
+        public Money Subtract(Money money)
+        {
+            return new Money(Amount - money.Amount);
+        }
+    }
+}
---
+++ MonopolyKataTests/MoneyTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MonopolyKata;
+
+namespace MonopolyKataTests
+{
+    [TestClass]
+    public class MoneyTests
+    {
+        [TestMethod]
+        public void AddMoneyReturnsCorrectAmount()
+        {
+            var money = new Money(5);
+            var moneyToAdd = new Money(4);
+            var expectedMoney = new Money(9);
+
+            var result = money.Add(moneyToAdd);
+
+            Assert.AreEqual(expectedMoney, result);
+        }
+
+        [TestMethod]
+        public void SubtractMoneyReturnsCorrectAmount()
+        {
+            var money = new Money(5);
+            var moneyToSubtract = new Money(4);
+            var expectedMoney = new Money(1);
+
+            var result = money.Subtract(moneyToSubtract);
+
+            Assert.AreEqual(expectedMoney, result);
+        }
+    }
+}
