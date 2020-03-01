933bf3fc9695cbf7ffeec9b7b45cf00e234e5a2a
Tripp Shelnutt <shelnutt77@gmail.com>
2020-03-01 17:57:17 -0500

    added property and property group

---
+++ MonopolyKata/Property.cs
@@ -0,0 +1,23 @@
+﻿namespace MonopolyKata
+{
+    public readonly struct Property
+    {
+        public Property(Location location, PropertyGroup propertyGroup, Money cost, Money rent)
+        {
+            Location = location;
+            PropertyGroup = propertyGroup;
+            Cost = cost;
+            Rent = rent;
+        }
+
+        public Location Location { get; }
+        public PropertyGroup PropertyGroup { get; }
+        public Money Cost { get; }
+        public Money Rent { get; }
+
+        public override string ToString()
+        {
+            return $"Property: {Location}";
+        }
+    }
+}
---
+++ MonopolyKata/PropertyGroup.cs
@@ -0,0 +1,17 @@
+﻿namespace MonopolyKata
+{
+    public readonly struct PropertyGroup
+    {
+        public PropertyGroup(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get; }
+
+        public override string ToString()
+        {
+            return $"PropertyGroup: {Value}";
+        }
+    }
+}
